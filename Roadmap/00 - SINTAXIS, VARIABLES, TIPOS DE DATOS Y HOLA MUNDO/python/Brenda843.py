499687ace9e0d34a538f5ce6fe79fda9821e448f
Brenda843 <142068588+Brenda843@users.noreply.github.com>
2024-11-21 16:42:06 -0300

    Create Brenda843.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Brenda843.py
@@ -0,0 +1,20 @@
+# https://python.org
+
+#Comentario en una línea 
+
+"""
+Esto también es 
+un cometario
+en varias línea
+"""
+
+'''
+Esto también es 
+un cometario
+en varias línea
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #por convención 
